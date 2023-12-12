ed0275cf33b55ab99aefa8909d2f27f741f5a69a
Mark Dunne <Markdunne117@gmail.com>
2023-12-12 13:05:46 +0000

    Finish product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,20 +10,39 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        # Gets product and id
-        product = get_object_or_404(Product, pk=item_id)
-        # Adds quantity multiplied by price to total
-        total += quantity * product.price
-        # Increment product count by quantity
-        product_count += quantity
-        # Adding dictionary to list of bag items
-        # Product object is added to give access to other product fields such as product.image etc
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        # Only execute this code if the item has no sizes
+        # Checks to see if item data is an integer, if it
+        # is, we are dealing with the quantity only.
+        if isinstance(item_data, int):
+            # Gets product and id
+            product = get_object_or_404(Product, pk=item_id)
+            # Adds quantity multiplied by price to total
+            total += item_data * product.price
+            # Increment product count by quantity
+            product_count += item_data
+            # Adding dictionary to list of bag items
+            # Product object is added to give access to other product fields such as product.image etc
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        # If item has a size, we need to iterate through a dictionary
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            # Iterate through inner dictionary of items_by_size
+            for size, quantity in item_data['items_by_size'].items():
+                # incrementing product and total count accordingly
+                total += quantity * product.price
+                product_count += quantity
+                # Add size the bag_items returned to the template
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+            })
 
     # Calculates delivery under threshold by multiplying
     # standard delivery percentage by total price
--- bag/views.py
+++ bag/views.py
@@ -15,6 +15,12 @@ def add_to_bag(request, item_id):
     # Gets quantity as an integer and redirect url from form
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    # If the product size is in the request,
+    # the size variable will be set to that value
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     # Stores users shopping bag contents in browser session so
     # that they can browse the site while the contents are saved.
     
@@ -23,15 +29,33 @@ def add_to_bag(request, item_id):
     # an empty dictionary if it doesnt.
     bag = request.session.get('bag', {})
 
-    # If there is already an id in the bag dictionary matching the product
-    # id selected by the user, the quantity value in incremented
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    # creating key of products id and set equal to quantity
+    if size:
+        # If the item is already in the bag, we check to see if an item with the
+        # same id and size already exists, and if so, increment the quantity for
+        # that size. Otherwise, set it equal to the quantity since the item already
+        # exists in the bag but this is a new size for that item.
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        # If the item is not already in the bag, it is added as a dictionary
+        # with the key of items_by_size as there may be multiple items with
+        # the same id but a different size. This allows us to structure the
+        # bag so that we can have a single item id for each item but still
+        # track multiple sizes.
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    # If there is no size, this code is run
     else:
-        bag[item_id] = quantity
+        # If there is already an id in the bag dictionary matching the product
+        # id selected by the user, the quantity value in incremented
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        # creating key of products id and set equal to quantity
+        else:
+            bag[item_id] = quantity
 
     # overwriting bag variable in session
     request.session['bag'] = bag
     return redirect(redirect_url)
-    
