a177111160aac76550520ce4c10184661ca14f74
Mark Dunne <Markdunne117@gmail.com>
2024-01-21 20:18:39 +0000

    Add admin product form

---
+++ products/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Product, Category
+
+# Product form class that extents the model form
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        # Dunder string used to include all the strings
+        fields = '__all__'
+
+    # Override init method to make changes to the fields
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Get all categories
+        categories = Category.objects.all()
+        # Use a list comprehension to create a list of tuples of
+        # the friendly names associated with their category id's.
+        # List comprehension is shorthand for loop used to add items to a list.
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        # Update category field on the form to
+        # use friendly names instead of id's.
+        self.fields['category'].choices = friendly_names
+        # Iterate through the fields and set classes
+        # to match the theme of the rest of site.
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
