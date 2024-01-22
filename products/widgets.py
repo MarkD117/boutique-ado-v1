276fa3f079166f98a7f801461df8a75618d7a1ef
Mark Dunne <Markdunne117@gmail.com>
2024-01-22 16:41:06 +0000

    Update image field display

--- products/widgets.py
+++ products/widgets.py
@@ -3,6 +3,7 @@ from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
 
 
+# Custom django widget used to beautify image selection on forms
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
