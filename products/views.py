735aec5ea10dfa64bc5665cb0ce093d22f0538c6
Mark Dunne <Markdunne117@gmail.com>
2024-01-22 11:46:36 +0000

    Add edit product functionality

--- products/views.py
+++ products/views.py
@@ -90,6 +90,7 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
 def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
@@ -110,6 +111,36 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    # Pre fill form by getting the product
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        # Instantiate a form using request.POST and request.FILES,
+        # and tell it the specific instance we want to update is 
+        # the product that is obtained above.
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            # Redirect to the product detail page using the product id
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        # Info message letting user know they are editing a product
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
