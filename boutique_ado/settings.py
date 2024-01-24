9714ab5843f09272d1e4a5b961efbd9c98308714
Mark Dunne <Markdunne117@gmail.com>
2024-01-24 20:40:54 +0000

    Update allowed hosts in settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['boutique-ado-md.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['boutique-ado-md-63e3802e571f.herokuapp.com','localhost']
 
 
 # Application definition
